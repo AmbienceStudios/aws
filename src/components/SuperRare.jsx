3c1b842968c49da3f02279fc3f6f59af089428c8
Freequency38 <Stanton.Deron@icloud.com>
2022-07-17 16:09:44 -0700

    Updated and Added Boxers

--- src/components/SuperRare.jsx
+++ src/components/SuperRare.jsx
@@ -1,159 +1,42 @@
 import React from "react";
 import Card from "./Card";
-import super1 from "../assets/Boxer3.jpg";
-import super2 from "../assets/Boxer4_1.jpg";
-import super3 from "../assets/Boxer6_1.jpg";
-import super4 from "../assets/Boxer7.jpg";
-import super5 from "../assets/Boxer8.jpg";
-import super6 from "../assets/Boxer10.jpg";
-import super7 from "../assets/Boxer14.jpg";
-import super8 from "../assets/Boxer15.jpg";
-import super9 from "../assets/boxer_14_laker.jpg";
-import super10 from "../assets/boxer_15_metallic_red.jpg";
-import super11 from "../assets/boxer_enlight.jpg";
+import Boxer1 from '../assets/Boxer1.jpg';
+import Boxer2 from "../assets/Boxer2.jpg";
+import Boxer3 from "../assets/Boxer3.jpg";
+import Boxer4 from "../assets/Boxer4.jpg";
+import Boxer5 from "../assets/Boxer5.jpg";
+import Boxer6 from "../assets/Boxer6.jpg";
+import Boxer7 from "../assets/Boxer7.jpg";
+import Boxer8 from "../assets/Boxer8.jpg";
+import Boxer9 from "../assets/Boxer9.jpg";
+import Boxer10 from "../assets/Boxer10.jpg";
+import Boxer11 from "../assets/Boxer11.jpg";
+import Boxer12 from "../assets/Boxer12.jpg";
 
-
-
-
-
- 
-export default function SuperRare() {
-  const data = [
-    {
-      image: super8,
-      series: "1st Gen",
-      title: "Golden Boy",
-      price: 4.24,
-      tag: 12983,
-      time: 1,
-    },
-    {
-      image: super1,
-      series: "1st Gen",
-      title: "Golden Boy",
-      price: 4.39,
-      tag: 12983,
-      time: 1,
-    },
-    {
-      image: super7,
-      series: "1st Gen",
-      title: "Golden Boy",
-      price: 4.89,
-      tag: 12983,
-      time: 1,
-    },
-    {
-      image: super3,
-      series: "1st Gen",
-      title: "Golden Boy",
-      price: 4.36,
-      tag: 12983,
-      time: 1,
-    },
-    // {
-    //   image: super5,
-    //   series: "1st Gen",
-    //   title: "Golden Boy",
-    //   price: 4.24,
-    //   tag: 12983,
-    //   time: 1,
-    // },
-    // {
-    //   image: super6,
-    //   series: "1st Gen",
-    //   title: "Golden Boy",
-    //   price: 4.39,
-    //   tag: 12983,
-    //   time: 1,
-    // },
-    // {
-    //   image: super7,
-    //   series: "1st Gen",
-    //   title: "Golden Boy",
-    //   price: 4.89,
-    //   tag: 12983,
-    //   time: 1,
-    // },
-    // {
-    //   image: super8,
-    //   series: "1st Gen",
-    //   title: "Golden Boy",
-    //   price: 4.36,
-    //   tag: 12983,
-    //   time: 1,
-    // },
-    // {
-    //   image: super9,
-    //   series: "1st Gen",
-    //   title: "Golden Boy",
-    //   price: 4.39,
-    //   tag: 12983,
-    //   time: 1,
-    // },
-    // {
-    //   image: super10,
-    //   series: "1st Gen",
-    //   title: "Golden Boy",
-    //   price: 4.89,
-    //   tag: 12983,
-    //   time: 1,
-    // },
-    // {
-    //   image: super11,
-    //   series: "1st Gen",
-    //   title: "Golden Boy",
-    //   price: 4.36,
-    //   tag: 12983,
-    //   time: 1,
-    // },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  ];
+const SuperRare = () => {
   return (
-    <div className="super-rare">
-      <div className="title-container">
-        <h2 className="title">NFT Collection Teaser</h2>
-        <p className="description">
-          Preview of the Golden Boy Collection! 
-        </p>
-      </div>
-      <div className="cards">
-        {data.map(({ image, series, title, price, tag, time }, index) => (
-          <Card
-            image={image}
-            series={series}
-            title={title}
-            // price={price}
-            // tag={tag}
-            // time={time}
-            // key={index}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    
+  <div class="img-gallery">
+    <img src={Boxer1}/>
+    <img src={Boxer2}/>
+    <img src={Boxer3}/>
+    <img src={Boxer4}/>
+    <img src={Boxer5}/>
+    <img src={Boxer6}/>
+    <img src={Boxer7}/>
+    <img src={Boxer8}/>
+    <img src={Boxer9}/>
+    <img src={Boxer10}/>
+    <img src={Boxer11}/>
+    <img src={Boxer12}/>
+ </div>
+
+
+  )
 }
+
+export default SuperRare
+
+
+
+
