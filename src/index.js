b3f1dde04bbf456fec715005a7ed95c23c587bb8
Freequency38 <Stanton.Deron@icloud.com>
2022-07-19 01:23:51 -0700

    Test 3D

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-// import Home from "./components/Home.jsx";
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Home/> */}
     <App />
   </React.StrictMode>,
   document.getElementById("root")
