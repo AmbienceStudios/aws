cbece743d1815cdf82a364973dbeac6d07206ceb
Freequency38 <Stanton.Deron@icloud.com>
2022-07-21 13:03:48 -0700

    lets see

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-// import Client from "./client"
+
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Client/> */}
     <App />
   </React.StrictMode>,
   document.getElementById("root")
